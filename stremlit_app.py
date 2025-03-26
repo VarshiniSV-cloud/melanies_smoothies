f536fe23d8b3484c2c5fca3f93de98e01416e26c
VarshiniSV-cloud <varshiniS3@hexaware.com>
2025-03-26 13:26:41 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -11,5 +11,17 @@ st.write("Customize your smoothie here:")
 name_on_order=st.text_input("Name on Smoothie")
 st.write("The name on Smoothie will be:", name_on_order)
 
+from snowflake.snowpark import Session
+
+session = Session.builder.configs({
+  account = "JAZZBKK-FIB07714"
+  user = "VARSHINIS"
+  password = "Varshinigopal12"
+  role = "SYSADMIN"
+  warehouse = "COMPUTE_WH"
+  database = "SMOOTHIES"
+  schema = "PUBLIC"
+}).create()
+
 cnx = st.connection("Snowflake")
 session = cnx.session()
