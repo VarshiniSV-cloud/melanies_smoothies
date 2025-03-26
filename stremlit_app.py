e17a980223fb30b915fcbef1d718d767a7fa2e7b
VarshiniSV-cloud <varshiniS3@hexaware.com>
2025-03-26 14:20:42 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -14,3 +14,27 @@ st.write("The name on Smoothie will be:", name_on_order)
 conn = st.connection("snowflake")
 session = conn.session()
 st.write("Connected to Snowflake:")
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list=st.multiselect('chose upto 5 fruits',my_dataframe,max_selections=5);
+
+if ingredients_list:
+
+    ingredients_string=''
+    for fruit_chosen in ingredients_list:
+        ingredients_string+=fruit_chosen+' '
+        
+    st.write(ingredients_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+
+    #st.write(my_insert_stmt)
+    #st.stop()
+    
+    time_to_insert= st.button('Submit')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
